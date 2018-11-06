693bf569b7cd6b4a6f8d6146371b968670954ab3
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-11-07 00:46:17 +0900

    Add Vector3

---
+++ src/Vector3.ts
@@ -0,0 +1,27 @@
+class Vector3 {
+  public _x = 0;
+  get x() {
+    return this._x;
+  }
+  set x(value) {
+    this._x = value;
+  }
+
+  public _y = 0;
+  get y() {
+    return this._y;
+  }
+  set y(value) {
+    this._y = value;
+  }
+
+  public _z = 0;
+  get z() {
+    return this._z;
+  }
+  set z(value) {
+    this._z = value;
+  }
+}
+
+export default Vector3;
